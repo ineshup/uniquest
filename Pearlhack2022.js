ad7134b53f57b4abc23c0fb58bdb14b5e779f0cb
Ines Binta Mouliom <75034151+ineshup@users.noreply.github.com>
2022-02-20 09:37:32 -0500

    Add files via upload

--- Pearlhack2022.js
+++ Pearlhack2022.js
@@ -1,22 +1,7 @@
-
-/* For dropdown for Quest Search - When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
 }
 
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
-} 
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
